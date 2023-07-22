86777fc6da1c999edaad36c9300347f841c3bbdb
huu.pb <huu.pb@teko.vn>
2023-07-22 18:22:56 +0700

    add ui for chat

--- MVVM/ViewModel/MainViewModel.cs
+++ MVVM/ViewModel/MainViewModel.cs
@@ -1,4 +1,5 @@
-﻿using SecretMessage.MVVM.Model;
+﻿using SecretMessage.Core;
+using SecretMessage.MVVM.Model;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -8,15 +9,42 @@ using System.Threading.Tasks;
 
 namespace SecretMessage.MVVM.ViewModel
 {
-    public class MainViewModel
+    public class MainViewModel : ObservableObject
     {
         public ObservableCollection<MessageModel> Messages { get; set; }
         public ObservableCollection<ContactModel> Contacts { get; set; }
+        /* Commands */
+        public RelayCommand SendCommand { get; set; }
+        private ContactModel _selectedContact;
+        public ContactModel SelectedContact 
+        {
+            get { return _selectedContact; }
+            set { _selectedContact = value;
+                OnPropertyChanged();
+            }
+        }
+        private string _message;
+        public string Message
+        {
+            get { return _message; }
+            set { _message = value;
+                OnPropertyChanged();
+            }
+
+        }
         public MainViewModel()
         {
             Messages = new ObservableCollection<MessageModel>();
             Contacts = new ObservableCollection<ContactModel>();
-
+            SendCommand = new RelayCommand(o => 
+            {
+                Messages.Add(new MessageModel
+                {
+                    Message=Message,
+                    FirstMessage=false
+                });
+                Message = "";
+            });
             Messages.Add(new MessageModel
             {
                 Username = "Khai",
